c995322e22525cd830f0c6d054b127fc205ec90a
juanzjck <juanzjck1996@gmail.com>
2018-09-07 07:58:40 -0500

    Coneccion base de datos

--- PostAPI.py
+++ PostAPI.py
@@ -1,65 +1,19 @@
-import json
-import js2py
-import sys
-import falcon
-from Naked.toolshed.shell import execute_js, muterun_js
-from twilio import twiml
-from flask import  Flask, request
-from flask_restful import  Api, Resource, reqparse
+
+#dependens
+from flask import  Flask
+from flask_restful import  Api
+from Classes import DataBase
+from Classes.DataBase import DataBase
 app=Flask(__name__)
 api=Api(app)
-db = [{"Valor":"2","idpos":"12sd","huella":"adasd"},{"Valor":"20","idpos":"12sd","huella":"sdas334"}]
+#clasess
+from Classes import Pos
+from Classes.Pos import Pos
+def main():
+    db = DataBase("Jack1996jack", "IdPayer")
+    #paths
+    api.add_resource(Pos, '/pos/<string:option>',resource_class_kwargs={ 'connection': db })
+    app.run(debug=True)
 
-class pos(Resource):
+if __name__ == "__main__": main()
 
-
-    # print(str(decoded["Pos"][0]["Info"][0]["Valor"]) + str(decoded["Pos"][0]["Info"][0]["id_pos"]) + str(
-    # decoded["Pos"][0]["Info"][0]["huella"]))
-
-    #resive el jsnon lo lee y lo interpreta con javascript
-
-
-    def post(self, data):
-        parser= reqparse.RequestParser()
-        parser.add_argument("Valor")
-        parser.add_argument("idpos")
-        parser.add_argument("huella")
-        args= parser.parse_args()
-        resp=''
-        entry_pos={"Valor":args["Valor"],"idpos":args["idpos"],"huella":args["huella"]}
-       # db.append(entry_pos)
-        for d in db:
-            #print(d["Valor"])
-            if d["huella"] == args["huella"]:
-
-                resp='Succesful,valor:'+args["Valor"]
-            else:
-
-             resp = 'fail'
-
-
-
-        # encoded
-        # data_string = json.dumps(data)
-
-        # Decoded
-        #decoded = json.loads(data)
-        print(data)
-        print(resp)
-        #print(str(decoded["Pos"][0]["Info"][0]["Valor"]))
-        #===========
-        #response = muterun_js('Xchange.js')
-        # if response.exitcode == 0:
-        #     print(response.stdout)
-        # else:
-        #    sys.stderr.write(response.stderr)
-        #===========
-
-
-        return resp
-class posr(Resource):
-    def hola(self):
-        print('hola')
-api.add_resource(pos,'/pos/<string:data>')
-api.add_resource(posr,'/pos2/<string:data>')
-app.run(debug=True)
--- request.py
+++ request.py
@@ -1,18 +1,27 @@
 import requests
 import json
 
+# importing the requests library
+import requests
+
+# api-endpoint
+
+
 headers={'Content-type': 'application/json', 'Accept': 'text/plain'}
-#url_api='http://127.0.0.1:5000/1'
-url_api='http://127.0.0.1:5000/pos2/4'
+#url_api='http://192.168.40.57:5000/1'
+url_api='http://127.0.0.1:5000/pos/4'
 
 
-content_json={"Valor":"79","idpos":"asd","huella":"sdas334"}
+content_json={"Valor":"79","idpos":"asd","huella":"sdas334","PIN":"0224"}
 content=json.dumps(content_json)
 #resp = requests.get(url_api,headers=header)
 
 resp =requests.post(url_api, data=content_json)
-resp.status_code
-resp.headers['content-type']
-resp.encoding
-resp.json()
+#resp.status_code
+#200
+#resp.headers['content-type']
+#text/json
+#resp.encoding
+#resp.json()
+#mensaje en jsnon
 print(resp.json())
